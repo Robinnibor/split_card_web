ab996138010331b1691a6668dccca60d9d4f9362
robinnibor <robinksskss@gmail.com>
2023-06-15 14:25:13 +0800

    fix: use PORT env

--- backend/split.py
+++ backend/split.py
@@ -6,9 +6,6 @@ from find_cards import find_card_positions
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
 
 @app.route('/get_cards', methods=['POST'])
 def get_cards():
@@ -18,7 +15,7 @@ def get_cards():
     image = cv2.resize(image, (image.shape[1]*scale_factor, image.shape[0]*scale_factor))
     scaled_cards = find_card_positions(image)
     original_cards = [(x//scale_factor, y//scale_factor, w//scale_factor, h//scale_factor) for (x, y, w, h) in scaled_cards]
-    print('orig cards = ', original_cards)
+    # print('orig cards = ', original_cards)
     return jsonify({'original_cards': original_cards, 'scaled_cards': scaled_cards, 'scale_factor': scale_factor})
 
 @app.route('/upload_image', methods=['POST'])
@@ -46,4 +43,5 @@ def upload_image():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    print('port = ',port )
     app.run(host='0.0.0.0', port=port)
